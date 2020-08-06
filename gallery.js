4e298d68cb767a21b2d8a23a6f6cf741f025d4e6
Diptiranjan S <15139672+diptiranjans@users.noreply.github.com>
2020-08-06 15:10:41 +0530

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -1,4 +1,5 @@
-const gallery1 = "./images/gallery1.png";
+const gallery_1 = "./images/gallery1.png";
+const gallery_exp={};
 const gallery2 = "./images/gallery2.png";
 const gallery3 = "./images/gallery3.png";
 let gallery_flowers = [
